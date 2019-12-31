039794fa05ac33777f754131d463519f8ec36ef0
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 09:46:03 -0600

    Rewriting as Laminas Project package

--- src/Exception/RuntimeException.php
+++ src/Exception/RuntimeException.php
@@ -1,13 +1,14 @@
 <?php
+
 /**
- * @see       https://github.com/zendframework/zend-expressive-router for the canonical source repository
- * @copyright Copyright (c) 2015-2017 Zend Technologies USA Inc. (https://www.zend.com)
- * @license   https://github.com/zendframework/zend-expressive-router/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/mezzio/mezzio-router for the canonical source repository
+ * @copyright https://github.com/mezzio/mezzio-router/blob/master/COPYRIGHT.md
+ * @license   https://github.com/mezzio/mezzio-router/blob/master/LICENSE.md New BSD License
  */
 
 declare(strict_types=1);
 
-namespace Zend\Expressive\Router\Exception;
+namespace Mezzio\Router\Exception;
 
 class RuntimeException extends \RuntimeException implements ExceptionInterface
 {
--- test/Middleware/ImplicitHeadMiddlewareTest.php
+++ test/Middleware/ImplicitHeadMiddlewareTest.php
@@ -1,15 +1,20 @@
 <?php
+
 /**
- * @see       https://github.com/zendframework/zend-expressive-router for the canonical source repository
- * @copyright Copyright (c) 2018 Zend Technologies USA Inc. (https://www.zend.com)
- * @license   https://github.com/zendframework/zend-expressive-router/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/mezzio/mezzio-router for the canonical source repository
+ * @copyright https://github.com/mezzio/mezzio-router/blob/master/COPYRIGHT.md
+ * @license   https://github.com/mezzio/mezzio-router/blob/master/LICENSE.md New BSD License
  */
 
 declare(strict_types=1);
 
-namespace ZendTest\Expressive\Router\Middleware;
+namespace MezzioTest\Router\Middleware;
 
 use Fig\Http\Message\RequestMethodInterface as RequestMethod;
+use Mezzio\Router\Middleware\ImplicitHeadMiddleware;
+use Mezzio\Router\Route;
+use Mezzio\Router\RouteResult;
+use Mezzio\Router\RouterInterface;
 use PHPUnit\Framework\TestCase;
 use Prophecy\Argument;
 use Prophecy\Prophecy\ObjectProphecy;
@@ -17,10 +22,6 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\StreamInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Zend\Expressive\Router\Middleware\ImplicitHeadMiddleware;
-use Zend\Expressive\Router\Route;
-use Zend\Expressive\Router\RouteResult;
-use Zend\Expressive\Router\RouterInterface;
 
 class ImplicitHeadMiddlewareTest extends TestCase
 {
@@ -134,6 +135,7 @@ class ImplicitHeadMiddlewareTest extends TestCase
         $result = RouteResult::fromRoute($route->reveal());
 
         $request->withAttribute(RouteResult::class, $result)->will([$request, 'reveal']);
+        $request->withAttribute(\Zend\Expressive\Router\RouteResult::class, $result)->will([$request, 'reveal']);
 
         $this->router->match($request)->willReturn($result);
 
@@ -169,6 +171,7 @@ class ImplicitHeadMiddlewareTest extends TestCase
         $result = RouteResult::fromRoute($route->reveal(), ['foo' => 'bar', 'baz' => 'bat']);
 
         $request->withAttribute(RouteResult::class, $result)->will([$request, 'reveal']);
+        $request->withAttribute(\Zend\Expressive\Router\RouteResult::class, $result)->will([$request, 'reveal']);
         $request->withAttribute('foo', 'bar')->will([$request, 'reveal'])->shouldBeCalled();
         $request->withAttribute('baz', 'bat')->will([$request, 'reveal'])->shouldBeCalled();
 
