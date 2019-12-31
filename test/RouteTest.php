abb68f7f76c3d70b289aac5cdc9eba9524ac77f7
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 09:45:46 -0600

    Rewriting as Laminas Project package

--- test/RouteTest.php
+++ test/RouteTest.php
@@ -1,20 +1,21 @@
 <?php
+
 /**
- * @see       https://github.com/zendframework/zend-expressive-router for the canonical source repository
- * @copyright Copyright (c) 2015-2016 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   https://github.com/zendframework/zend-expressive-router/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/mezzio/mezzio-router for the canonical source repository
+ * @copyright https://github.com/mezzio/mezzio-router/blob/master/COPYRIGHT.md
+ * @license   https://github.com/mezzio/mezzio-router/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Expressive\Router;
+namespace MezzioTest\Router;
 
 use Fig\Http\Message\RequestMethodInterface as RequestMethod;
 use Interop\Http\ServerMiddleware\MiddlewareInterface;
+use Mezzio\Router\Exception\InvalidArgumentException;
+use Mezzio\Router\Route;
 use PHPUnit_Framework_TestCase as TestCase;
-use Zend\Expressive\Router\Exception\InvalidArgumentException;
-use Zend\Expressive\Router\Route;
 
 /**
- * @covers \Zend\Expressive\Router\Route
+ * @covers \Mezzio\Router\Route
  */
 class RouteTest extends TestCase
 {
