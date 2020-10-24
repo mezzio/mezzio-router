e95ebe96a8196fe3a03f6e82dbf0167466722f96
Marcel Thole <marcel@marcelthole.de>
2020-10-24 17:06:01 +0200

    Restore renamed abstract ImplicitMethodsIntegrationTest class
    Signed-off-by: Marcel Thole <marcel@marcelthole.de>

---
+++ src/Test/ImplicitMethodsIntegrationTest.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @see       https://github.com/mezzio/mezzio-router for the canonical source repository
+ * @copyright https://github.com/mezzio/mezzio-router/blob/master/COPYRIGHT.md
+ * @license   https://github.com/mezzio/mezzio-router/blob/master/LICENSE.md New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace Mezzio\Router\Test;
+
+// phpcs:ignoreFile
+
+/**
+ * @deprecated Use AbstractImplicitMethodsIntegrationTest instead
+ */
+abstract class ImplicitMethodsIntegrationTest extends AbstractImplicitMethodsIntegrationTest
+{
+}
