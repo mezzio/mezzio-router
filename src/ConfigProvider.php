6e9f098f829882f41f482d6ca66d2398cb003c87
George Steel <george@net-glue.co.uk>
2023-01-10 22:37:59 +0000

    Stringify Zend aliases because none of the classes exist
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -19,12 +19,12 @@ class ConfigProvider
         return [
             // Legacy Zend Framework aliases
             'aliases' => [
-                \Zend\Expressive\Router\Middleware\DispatchMiddleware::class => Middleware\DispatchMiddleware::class,
-                \Zend\Expressive\Router\Middleware\ImplicitHeadMiddleware::class => Middleware\ImplicitHeadMiddleware::class,
-                \Zend\Expressive\Router\Middleware\ImplicitOptionsMiddleware::class => Middleware\ImplicitOptionsMiddleware::class,
-                \Zend\Expressive\Router\Middleware\MethodNotAllowedMiddleware::class => Middleware\MethodNotAllowedMiddleware::class,
-                \Zend\Expressive\Router\Middleware\RouteMiddleware::class => Middleware\RouteMiddleware::class,
-                \Zend\Expressive\Router\RouteCollector::class => RouteCollector::class,
+                'Zend\Expressive\Router\Middleware\DispatchMiddleware' => Middleware\DispatchMiddleware::class,
+                'Zend\Expressive\Router\Middleware\ImplicitHeadMiddleware' => Middleware\ImplicitHeadMiddleware::class,
+                'Zend\Expressive\Router\Middleware\ImplicitOptionsMiddleware' => Middleware\ImplicitOptionsMiddleware::class,
+                'Zend\Expressive\Router\Middleware\MethodNotAllowedMiddleware' => Middleware\MethodNotAllowedMiddleware::class,
+                'Zend\Expressive\Router\Middleware\RouteMiddleware' => Middleware\RouteMiddleware::class,
+                'Zend\Expressive\Router\RouteCollector' => RouteCollector::class,
             ],
             'factories' => [
                 Middleware\DispatchMiddleware::class         => Middleware\DispatchMiddlewareFactory::class,
