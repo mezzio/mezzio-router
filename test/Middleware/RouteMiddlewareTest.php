62328b4e84aca4bb4cfa6a2edccda351b4ff2ad2
George Steel <george@net-glue.co.uk>
2023-02-25 11:28:40 +0000

    Reduce mocking, fix calls to `MockObject::withConsecutive()`
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- test/Middleware/RouteMiddlewareTest.php
+++ test/Middleware/RouteMiddlewareTest.php
@@ -4,143 +4,98 @@ declare(strict_types=1);
 
 namespace MezzioTest\Router\Middleware;
 
+use Laminas\Diactoros\Response\TextResponse;
+use Laminas\Diactoros\ServerRequest;
 use Mezzio\Router\Middleware\RouteMiddleware;
 use Mezzio\Router\Route;
 use Mezzio\Router\RouteResult;
 use Mezzio\Router\RouterInterface;
+use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
-use Psr\Http\Server\RequestHandlerInterface;
 
-/** @covers \Mezzio\Router\Middleware\RouteMiddleware */
+#[CoversClass(RouteMiddleware::class)]
 final class RouteMiddlewareTest extends TestCase
 {
-    /** @var RouterInterface&MockObject */
-    private RouterInterface $router;
-
-    /** @var ResponseInterface&MockObject */
-    private ResponseInterface $response;
-
-    /** @var ServerRequestInterface&MockObject */
-    private ServerRequestInterface $request;
-
-    /** @var RequestHandlerInterface&MockObject */
-    private RequestHandlerInterface $handler;
-
+    private RouterInterface&MockObject $router;
     private RouteMiddleware $middleware;
 
     protected function setUp(): void
     {
         parent::setUp();
 
-        $this->router   = $this->createMock(RouterInterface::class);
-        $this->request  = $this->createMock(ServerRequestInterface::class);
-        $this->response = $this->createMock(ResponseInterface::class);
-        $this->handler  = $this->createMock(RequestHandlerInterface::class);
-
+        $this->router     = $this->createMock(RouterInterface::class);
         $this->middleware = new RouteMiddleware($this->router);
     }
 
     public function testRoutingFailureDueToHttpMethodCallsHandlerWithRequestComposingRouteResult(): void
     {
-        $result = RouteResult::fromRouteFailure(['GET', 'POST']);
+        $result   = RouteResult::fromRouteFailure(['GET', 'POST']);
+        $response = new TextResponse('Whatever');
+        $handler  = new RequestHandlerStub($response);
+        $request  = new ServerRequest();
 
         $this->router
             ->method('match')
-            ->with($this->request)
+            ->with($request)
             ->willReturn($result);
 
-        $this->handler
-            ->method('handle')
-            ->with($this->request)
-            ->willReturn($this->response);
-
-        $this->request
-            ->expects(self::once())
-            ->method('withAttribute')
-            ->withConsecutive(
-                [
-                    RouteResult::class,
-                    $result,
-                ]
-            )->willReturnSelf();
-
-        $response = $this->middleware->process($this->request, $this->handler);
-
-        self::assertSame($this->response, $response);
+        self::assertSame($response, $this->middleware->process($request, $handler));
+        self::assertTrue($handler->didExecute());
+        $received = $handler->receivedRequest();
+        self::assertNotSame($request, $received);
+        self::assertSame($result, $received->getAttribute(RouteResult::class));
     }
 
     public function testGeneralRoutingFailureInvokesHandlerWithRequestComposingRouteResult(): void
     {
         $result = RouteResult::fromRouteFailure(null);
 
+        $response = new TextResponse('Whatever');
+        $handler  = new RequestHandlerStub($response);
+        $request  = new ServerRequest();
+
         $this->router
             ->method('match')
-            ->with($this->request)
+            ->with($request)
             ->willReturn($result);
 
-        $this->handler
-            ->method('handle')
-            ->with($this->request)
-            ->willReturn($this->response);
-
-        $this->request
-            ->expects(self::exactly(1))
-            ->method('withAttribute')
-            ->withConsecutive(
-                [
-                    RouteResult::class,
-                    $result,
-                ]
-            )->willReturnSelf();
-
-        $response = $this->middleware->process($this->request, $this->handler);
-
-        self::assertSame($this->response, $response);
+        self::assertSame($response, $this->middleware->process($request, $handler));
+        self::assertTrue($handler->didExecute());
+        $received = $handler->receivedRequest();
+        self::assertNotSame($request, $received);
+        self::assertSame($result, $received->getAttribute(RouteResult::class));
     }
 
     public function testRoutingSuccessInvokesHandlerWithRequestComposingRouteResultAndAttributes(): void
     {
-        $middleware = $this->createMock(MiddlewareInterface::class);
-        $result     = RouteResult::fromRoute(
-            new Route('/foo', $middleware),
-            ['foo' => 'bar', 'baz' => 'bat']
+        $result = RouteResult::fromRoute(
+            new Route(
+                '/foo',
+                $this->createMock(MiddlewareInterface::class)
+            ),
+            ['foo' => 'bar', 'baz' => 'bat'],
         );
+
+        $response = new TextResponse('Whatever');
+        $handler  = new RequestHandlerStub($response);
+        $request  = new ServerRequest();
 
         $this->router
             ->expects(self::once())
             ->method('match')
-            ->with($this->request)
+            ->with($request)
             ->willReturn($result);
 
-        $this->request
-            ->expects(self::exactly(3))
-            ->method('withAttribute')
-            ->withConsecutive(
-                [
-                    RouteResult::class,
-                    $result,
-                ],
-                [
-                    'foo',
-                    'bar',
-                ],
-                [
-                    'baz',
-                    'bat',
-                ]
-            )->willReturnSelf();
-
-        $this->handler
-            ->method('handle')
-            ->with($this->request)
-            ->willReturn($this->response);
-
-        $response = $this->middleware->process($this->request, $this->handler);
-
-        self::assertSame($this->response, $response);
+        self::assertSame(
+            $response,
+            $this->middleware->process($request, $handler),
+        );
+        self::assertTrue($handler->didExecute());
+        $received = $handler->receivedRequest();
+        self::assertNotSame($request, $received);
+        self::assertSame('bar', $received->getAttribute('foo'));
+        self::assertSame('bat', $received->getAttribute('baz'));
     }
 }
