ffd5b43a567a32cacb40c8e955b0c55e7ca92380
George Steel <george@net-glue.co.uk>
2023-03-28 17:03:18 +0100

    Adds a container integration test
    This test case ensures that a configured container can return everything declared by the config provider.
    
    Signed-off-by: George Steel <george@net-glue.co.uk>

---
+++ test/ServiceManagerIntegrationTest.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MezzioTest\Router;
+
+use Laminas\ServiceManager\ConfigInterface;
+use Laminas\ServiceManager\ServiceManager;
+use Mezzio\Router;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\StreamInterface;
+
+use function array_merge_recursive;
+
+/** @psalm-import-type ServiceManagerConfigurationType from ConfigInterface */
+final class ServiceManagerIntegrationTest extends TestCase
+{
+    private ContainerInterface $container;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        /** @psalm-var ServiceManagerConfigurationType $config */
+        $config          = array_merge_recursive(
+            (new Router\ConfigProvider())->getDependencies(),
+            [
+                'factories' => [
+                    ResponseFactoryInterface::class => function (): ResponseFactoryInterface {
+                        return $this->createMock(ResponseFactoryInterface::class);
+                    },
+                    Router\RouterInterface::class   => function (): Router\RouterInterface {
+                        return $this->createMock(Router\RouterInterface::class);
+                    },
+                    StreamInterface::class          => function (): callable {
+                        return fn (): StreamInterface => $this->createMock(StreamInterface::class);
+                    },
+                ],
+            ],
+        );
+        $this->container = new ServiceManager($config);
+    }
+
+    /**
+     * A list of container ids that should resolve to the expected type
+     *
+     * @return array<array-key, array{0: class-string, 1: class-string}>
+     */
+    public static function factories(): array
+    {
+        return [
+            [Router\Middleware\DispatchMiddleware::class, Router\Middleware\DispatchMiddleware::class],
+            [Router\Middleware\ImplicitHeadMiddleware::class, Router\Middleware\ImplicitHeadMiddleware::class],
+            [Router\Middleware\ImplicitOptionsMiddleware::class, Router\Middleware\ImplicitOptionsMiddleware::class],
+            [Router\Middleware\MethodNotAllowedMiddleware::class, Router\Middleware\MethodNotAllowedMiddleware::class],
+            [Router\Middleware\RouteMiddleware::class, Router\Middleware\RouteMiddleware::class],
+            [Router\RouteCollector::class, Router\RouteCollector::class],
+        ];
+    }
+
+    /**
+     * A list of container ids that should resolve to the expected type
+     *
+     * @return array<array-key, array{0: class-string, 1: class-string}>
+     */
+    public static function aliases(): array
+    {
+        return [
+            [Router\RouteCollectorInterface::class, Router\RouteCollectorInterface::class],
+        ];
+    }
+
+    /**
+     * @param class-string $id
+     * @param class-string $expectedType
+     */
+    #[DataProvider('factories')]
+    public function testFactoryIdentifiersCanBeRetrievedFromTheContainer(string $id, string $expectedType): void
+    {
+        self::assertTrue($this->container->has($id));
+        $instance = $this->container->get($id);
+        self::assertInstanceOf($expectedType, $instance);
+    }
+
+    /**
+     * @param class-string $id
+     * @param class-string $expectedType
+     */
+    #[DataProvider('aliases')]
+    public function testAliasesCanBeRetrievedFromTheContainer(string $id, string $expectedType): void
+    {
+        self::assertTrue($this->container->has($id));
+        $instance = $this->container->get($id);
+        self::assertInstanceOf($expectedType, $instance);
+    }
+}
