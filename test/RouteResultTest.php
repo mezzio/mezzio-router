341a47e932d1a472348fcfdc93fade8a2766afc2
George Steel <george@net-glue.co.uk>
2025-10-11 10:13:37 +0100

    Refresh lock and baseline post merge up, + CS Fix
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- test/RouteResultTest.php
+++ test/RouteResultTest.php
@@ -10,13 +10,10 @@ use MezzioTest\Router\Asset\FixedResponseMiddleware;
 use MezzioTest\Router\Asset\NoOpMiddleware;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Depends;
-use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-
-use function assert;
 
 #[CoversClass(RouteResult::class)]
 final class RouteResultTest extends TestCase
